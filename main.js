237486efed3e51379807edf8bec5d48a77d004b3
Owen <zgaoguowen@gmail.com>
2019-05-12 23:47:56 +0800

    Add pages

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const express = require('express')
 const nunjucks = require('nunjucks') //模板引擎
 
 const app = express()
+const resolve = dir => path.resolve(__dirname,  dir)
 
-
+// 开放 public 资源
+app.use('/public/',express.static(resolve('./public/')))
 // views 在views 目录中查找静态资源
-nunjucks.configure(path.join(__dirname,'views'), {
+nunjucks.configure(resolve('views'), {
     autoescape: true,
     express: app,
     watch:true  //监听文件，预编译；生产环境关闭
@@ -17,6 +19,10 @@ app.get('/',(req,res,next) =>{
     // res.status(200).send('hello world Owen')
     // 第一个文件名，第二个传参数
     res.render('index.html',{name:'Owen'})
+})
+
+app.get('/people/home',(req,res,next) =>{
+    res.render('home.html')
 })
 
 app.listen(3001,() =>{
